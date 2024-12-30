902628b8e76c7c3de9c3e0083760fb42b4f4e0b5
Aum Bhatt <aum@deriv.com>
2024-12-30 15:50:46 +0400

    feat: move logout inside AccountSelector

--- src/components/AccountSelector.jsx
+++ src/components/AccountSelector.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { useAuth } from "../hooks/useAuth.jsx";
+import { Button } from "@deriv-com/quill-ui";
+import { LegacyLogout1pxIcon } from "@deriv/quill-icons";
 
-const AccountSelector = ({ defaultAccount, otherAccounts }) => {
+const AccountSelector = ({ defaultAccount, otherAccounts, onLogout }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedAccount, setSelectedAccount] = useState(defaultAccount);
     const { updateAccounts } = useAuth();
 
     const handleAccountSelect = (account) => {
         console.log("AccountSelector - Selecting account:", account);
-        const filteredOtherAccounts = allAccounts.filter(acc => acc.account !== account.account);
+        const filteredOtherAccounts = allAccounts.filter(
+            (acc) => acc.account !== account.account
+        );
         setSelectedAccount(account);
         console.log("AccountSelector - Calling updateAccounts with:", {
             newDefault: account,
-            newOthers: filteredOtherAccounts
+            newOthers: filteredOtherAccounts,
         });
         updateAccounts(account, filteredOtherAccounts);
         setIsOpen(false);
@@ -29,11 +33,13 @@ const AccountSelector = ({ defaultAccount, otherAccounts }) => {
             >
                 <span>
                     {selectedAccount.account}
-                    {selectedAccount.currency && `(${selectedAccount.currency})`}
+                    {selectedAccount.currency &&
+                        `(${selectedAccount.currency})`}
                 </span>
                 <svg
-                    className={`w-5 h-5 ml-2 transition-transform duration-200 ${isOpen ? "transform rotate-180" : ""
-                        }`}
+                    className={`w-5 h-5 ml-2 transition-transform duration-200 ${
+                        isOpen ? "transform rotate-180" : ""
+                    }`}
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
@@ -47,15 +53,16 @@ const AccountSelector = ({ defaultAccount, otherAccounts }) => {
             </button>
 
             {isOpen && (
-                <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg">
+                <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg p-2">
                     <ul className="py-1 max-h-60 overflow-auto">
                         {allAccounts.map((account) => (
                             <li
                                 key={account.account}
-                                className={`px-4 py-2 text-sm cursor-pointer hover:bg-gray-100 ${selectedAccount.account === account.account
-                                    ? "bg-gray-50 text-gray-900"
-                                    : "text-gray-700"
-                                    }`}
+                                className={`px-4 py-2 text-sm cursor-pointer hover:bg-gray-100 ${
+                                    selectedAccount.account === account.account
+                                        ? "bg-gray-100 text-gray-900"
+                                        : "text-gray-700"
+                                }`}
                                 onClick={() => handleAccountSelect(account)}
                             >
                                 {account.account}
@@ -63,6 +70,22 @@ const AccountSelector = ({ defaultAccount, otherAccounts }) => {
                             </li>
                         ))}
                     </ul>
+                    <div className="mt-2 flex justify-start">
+                        <Button
+                            onClick={onLogout}
+                            variant="secondary"
+                            color="black"
+                            size="sm"
+                            icon={
+                                <LegacyLogout1pxIcon
+                                    fill="#000000"
+                                    iconSize="xs"
+                                />
+                            }
+                        >
+                            Log out
+                        </Button>
+                    </div>
                 </div>
             )}
         </div>
@@ -80,6 +103,7 @@ AccountSelector.propTypes = {
             currency: PropTypes.string.isRequired,
         })
     ).isRequired,
+    onLogout: PropTypes.func.isRequired,
 };
 
 export default AccountSelector;
